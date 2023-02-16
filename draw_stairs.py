e698ef52f7115f401ec522a28626c7cfaa403edb
MaxRombach <rombach.maxime@gmail.com>
2023-02-16 17:02:33 +0100

    tolerance envelop around focal surface

--- draw_stairs.py
+++ draw_stairs.py
@@ -20,7 +20,7 @@ Z_data = -t['Z']
 CRD_data = t['CRD']
 R_data = t['R']
 
-R2Z = interp1d(R_data,Z_data,kind='cubic') #leave 'cubic' interpolation for normal vectors calculations
+R2Z = interp1d(R_data,Z_data,kind='cubic', fill_value = "extrapolate") #leave 'cubic' interpolation for normal vectors calculations
 R2CRD = interp1d(R_data,CRD_data,kind='cubic')
 r = np.linspace(0,vigR,1000)
 z = R2Z(r) # Calculate focal plane curve from csv data
@@ -60,7 +60,7 @@ def get_normals(r_modules, R2Z, h=0.001):
 
     return np.array(normal)
 
-def get_normals_angles(normal):
+def get_normals_angles(normal, print_data = True):
     """Calculates the angle between the normal vectors and the vertical"""
     angles = []
 
@@ -69,7 +69,8 @@ def get_normals_angles(normal):
         angles.append(theta)
     
     angles = np.array(angles)
-    print("angle to normal [deg]: ", np.around(np.degrees(angles),3))
+    if print_data:
+        print("angle to normal [deg]: ",   np.around(np.degrees(angles),3))
 
     return angles
   
@@ -91,24 +92,13 @@ def rotate_modules_2_surface(x_modules, y_modules, angles):
         p = np.array([px,py,1]).reshape(3,1)
         left_module = p - np.array([module_width/2,0,0]).reshape(3,1)
         right_module = p + np.array([module_width/2,0,0]).reshape(3,1)
-        # print(right_module)
 
         c,s = np.cos(theta), np.sin(theta)
-        R_theta = np.array([[c,-s,0],
-                            [s,c,0],
-                            [0,0,1]])
+        R_theta = np.array([[c,-s,0],[s,c,0],[0,0,1]]) #
         Rt_minus= np.array([[1,0,-px],[0,1,-py],[0,0,1]])
         Rt_plus= np.array([[1,0,px],[0,1,py],[0,0,1]])
         new_left_module.append(Rt_plus @ R_theta @ Rt_minus @ left_module)
         new_right_module.append(Rt_plus @ R_theta @ Rt_minus @ right_module)
-
-        # c,s = np.cos(theta), np.sin(theta)
-        # rot_matrix = np.array([c, -s,s, c]).reshape(2,2)
-        # M_stack = np.hstack((rot_matrix,p-rot_matrix.dot(p)))
-        # M = np.vstack((M_stack,np.array([0,0,1])))
-
-        # new_left_module.append(M.dot(left_module))
-        # new_right_module.append(M.dot(right_module))
 
     return np.array(new_left_module), np.array(new_right_module)
 
@@ -124,7 +114,20 @@ def draw_modules(new_left_module, new_right_module, draw=True):
             ax.plot((new_left_module[i][0],new_right_module[i][0]),
                     (new_left_module[i][1],new_right_module[i][1]))
 
-        
+def tolerance_envelop(r,R2Z,envelop_width):
+
+    normal_vectors = get_normals(r,R2Z)
+    normal_angles = get_normals_angles(normal_vectors, print_data = False)
+    dx, dy= np.sin(normal_angles)*envelop_width/2, np.cos(normal_angles)*envelop_width/2
+    
+    r_envelop_plus = r + dx
+    z_envelop_plus = z + dy
+
+    r_envelop_minus = r - dx
+    z_envelop_minus = z - dy
+
+    return r_envelop_plus, z_envelop_plus, r_envelop_minus, z_envelop_minus
+
 def draw_modules_width(x_modules):
     for i in range(len(x_modules)): # draw modules width
         x_min = x_modules[i]-module_width/2
@@ -161,15 +164,25 @@ def draw_R2CRD(r,R_data,CRD_data,R2CRD, draw=True):
     plt.ylabel('CRD [deg]')
     plt.grid()
 
+def draw_envelop(r_envelop_plus, z_envelop_plus, r_envelop_minus, z_envelop_minus, draw = True):
+    
+    if not draw:
+        return
+    
+    ax.plot(r_envelop_minus, z_envelop_minus)
+    ax.plot(r_envelop_plus, z_envelop_plus)
+
 x_modules, y_modules = calc_modules_pos(Rc, module_width, nb_modules, R2Z)
 normal = get_normals(x_modules, R2Z)
 angles = get_normals_angles(normal)
 new_left_module, new_right_module = rotate_modules_2_surface(x_modules, y_modules, angles)
+r_envelop_plus, z_envelop_plus, r_envelop_minus, z_envelop_minus = tolerance_envelop(r,R2Z,envelop_width = 0.1)
 # print(new_left_module)
 
 ## Plotting time ##
 
 fig, ax = plt.subplots(1,1,figsize=(15,2))
+# fig, ax = plt.subplots()
 
 plot_time = 20 #seconds
 is_timer = False
@@ -178,8 +191,9 @@ draw_normals(x_modules,y_modules,normal,length=10)
 # plt.plot(x_radius, y_radius, '-.',label="BFS")
 draw_modules_width(x_modules)
 draw_modules(new_left_module, new_right_module, draw = True)
-plt.plot(r,z,'--g',label="focal surface")
+plt.plot(r,z,'--g',label="focal surface", linewidth=0.5)
 draw_BFS(Rc,vigR, draw=False)
+draw_envelop(r_envelop_plus, z_envelop_plus, r_envelop_minus, z_envelop_minus)
 plt.legend()
 plt.title('Focal surface and module arrangement')
 plt.xlabel('R [mm]')
