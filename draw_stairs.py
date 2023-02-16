b9f23fea1703c5196affb7d8e29e8620e7876ac3
MaxRombach <rombach.maxime@gmail.com>
2023-02-16 14:23:20 +0100

    rotated modules tangent to focal surface

--- draw_stairs.py
+++ draw_stairs.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -13,12 +16,12 @@ module_width = 80 # Module width [mm]
 nb_modules = 8 #number of modules of one side of the axis
 
 t = Table.read('MM1536-cfg1-20210910.csv', comment='#')
-t1 = t['Z']
+Z_data = -t['Z']
 CRD_data = t['CRD']
-R = t['R']
+R_data = t['R']
 
-R2Z = interp1d(t['R'],t1,kind='cubic') #leave 'cubic' interpolation for normal vectors calculations
-R2CRD = interp1d(t['R'],CRD_data,kind='cubic')
+R2Z = interp1d(R_data,Z_data,kind='cubic') #leave 'cubic' interpolation for normal vectors calculations
+R2CRD = interp1d(R_data,CRD_data,kind='cubic')
 r = np.linspace(0,vigR,1000)
 z = R2Z(r) # Calculate focal plane curve from csv data
 
@@ -53,7 +56,7 @@ def get_normals(r_modules, R2Z, h=0.001):
         norm = math.hypot(dx, dy)
         dx /= norm
         dy /= norm
-        normal.append([dy, -dx])
+        normal.append([-dy, dx])
 
     return np.array(normal)
 
@@ -69,7 +72,7 @@ def get_normals_angles(normal):
     print("angle to normal [deg]: ", np.around(np.degrees(angles),3))
 
     return angles
-
+  
 def draw_normals(x_modules, y_modules, normal, length=10, draw=True):
     if not draw:
         return
@@ -80,14 +83,57 @@ def draw_normals(x_modules, y_modules, normal, length=10, draw=True):
             ax.plot((x0, x0+nx), (y0, y0+ny))
             # ax.plot((x0, x0), (y0, y0+1*length),'r--')
 
+def rotate_modules_2_surface(x_modules, y_modules, angles):
+    new_left_module = []
+    new_right_module = []
+
+    for px,py,theta in zip(x_modules,y_modules,angles):
+        p = np.array([px,py,1]).reshape(3,1)
+        left_module = p - np.array([module_width/2,0,0]).reshape(3,1)
+        right_module = p + np.array([module_width/2,0,0]).reshape(3,1)
+        # print(right_module)
+
+        c,s = np.cos(theta), np.sin(theta)
+        R_theta = np.array([[c,-s,0],
+                            [s,c,0],
+                            [0,0,1]])
+        Rt_minus= np.array([[1,0,-px],[0,1,-py],[0,0,1]])
+        Rt_plus= np.array([[1,0,px],[0,1,py],[0,0,1]])
+        new_left_module.append(Rt_plus @ R_theta @ Rt_minus @ left_module)
+        new_right_module.append(Rt_plus @ R_theta @ Rt_minus @ right_module)
+
+        # c,s = np.cos(theta), np.sin(theta)
+        # rot_matrix = np.array([c, -s,s, c]).reshape(2,2)
+        # M_stack = np.hstack((rot_matrix,p-rot_matrix.dot(p)))
+        # M = np.vstack((M_stack,np.array([0,0,1])))
+
+        # new_left_module.append(M.dot(left_module))
+        # new_right_module.append(M.dot(right_module))
+
+    return np.array(new_left_module), np.array(new_right_module)
+
+def draw_modules(new_left_module, new_right_module, draw=True):
+    if not draw:
+        return
+    for i in range(len(new_left_module)): # draw modules width
+        if i == 0:
+            # ax.hlines(y=y_modules[i], xmin=x_min, xmax=x_max, color='r', label="fiber tips line", linewidth=1.25)
+            ax.plot((new_left_module[i][0],new_right_module[i][0]),
+                    (new_left_module[i][1],new_right_module[i][1]), color='b', label="rotated modules", linewidth=1.25)
+        else:
+            ax.plot((new_left_module[i][0],new_right_module[i][0]),
+                    (new_left_module[i][1],new_right_module[i][1]))
+
+        
 def draw_modules_width(x_modules):
     for i in range(len(x_modules)): # draw modules width
         x_min = x_modules[i]-module_width/2
         x_max = x_modules[i]+module_width/2
         if i == 0:
-            ax.hlines(y=y_modules[i], xmin=x_min, xmax=x_max, color='r', label="fiber tips line", linewidth=1.25)
+            # ax.hlines(y=y_modules[i], xmin=x_min, xmax=x_max, color='r', label="fiber tips line", linewidth=1.25)
+            ax.plot((x_min,x_max),(y_modules[i], y_modules[i]), color='r', label="fiber tips line", linewidth=1.25)
         else:
-            ax.hlines(y=y_modules[i], xmin=x_min, xmax=x_max, color='r')
+            ax.plot((x_min,x_max),(y_modules[i], y_modules[i]), color='r')
 
 def draw_BFS(Rc,vigR, draw=False, full_curve = False):
     """Draws the Best Fit Sphere of the focal surface for comparison with the interpolated one
@@ -103,33 +149,35 @@ def draw_BFS(Rc,vigR, draw=False, full_curve = False):
 
     ax.plot(x,y,label='BFS',color='orange',linestyle='-.')
 
-def draw_R2CRD(r,R,CRD_data,R2CRD, draw=True):
+def draw_R2CRD(r,R_data,CRD_data,R2CRD, draw=True):
     if not draw:
         return
     fig = plt.figure(num='R2CRD')
     plt.plot(r,R2CRD(r),label='interpolation')
-    plt.scatter(R,CRD_data,marker='.',color='red',label='data')
+    plt.scatter(R_data,CRD_data,marker='.',color='red',label='data')
     plt.legend()
     plt.title('Chief Ray Deviation in terms of radial pos on focal surface')
     plt.xlabel('R [mm]')
     plt.ylabel('CRD [deg]')
     plt.grid()
 
-
 x_modules, y_modules = calc_modules_pos(Rc, module_width, nb_modules, R2Z)
 normal = get_normals(x_modules, R2Z)
 angles = get_normals_angles(normal)
+new_left_module, new_right_module = rotate_modules_2_surface(x_modules, y_modules, angles)
+# print(new_left_module)
 
 ## Plotting time ##
 
 fig, ax = plt.subplots(1,1,figsize=(15,2))
 
 plot_time = 20 #seconds
-is_timer = True
+is_timer = False
 
-draw_normals(x_modules,y_modules,normal,length=1.5*Rc)
+draw_normals(x_modules,y_modules,normal,length=10)
 # plt.plot(x_radius, y_radius, '-.',label="BFS")
 draw_modules_width(x_modules)
+draw_modules(new_left_module, new_right_module, draw = True)
 plt.plot(r,z,'--g',label="focal surface")
 draw_BFS(Rc,vigR, draw=False)
 plt.legend()
@@ -137,7 +185,7 @@ plt.title('Focal surface and module arrangement')
 plt.xlabel('R [mm]')
 plt.ylabel('Z [mm]')
 plt.grid()
-draw_R2CRD(r,R,CRD_data,R2CRD, draw=True)
+draw_R2CRD(r,R_data,CRD_data,R2CRD, draw=False)
 
 if is_timer:
     plt.show(block=False)
