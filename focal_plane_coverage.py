9c5646fe1ce4f647c904e70f22871d4a181789e4
MaxRombach <rombach.maxime@gmail.com>
2023-03-03 12:50:15 +0100

    coverage 1 module finished

--- focal_plane_coverage.py
+++ focal_plane_coverage.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import random
 import array as array
 
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, MultiPolygon
+from shapely.ops import unary_union
 from shapely.plotting import plot_polygon, plot_points
 
 ## Parameters ##
@@ -19,6 +20,30 @@ module_vertices_y = np.array([0,0,69.3,0])
 start_offset_x = 6.2
 start_offset_y = 3.41
 
+## Positioners numbering:
+            # 0: bottom left
+            # count a row then switch to left side of next one to continue
+            # last positoner: top one
+
+def remove_positioner(xx,yy, list_to_remove):
+     
+     xx_sliced = np.delete(xx,list_to_remove)
+     yy_sliced = np.delete(yy,list_to_remove)
+
+     return xx_sliced, yy_sliced
+
+
+def to_polygon_format(x,y):
+
+    coords = []
+    for (i,j) in zip(x,y):
+            coords.append((i,j))
+
+    return coords
+
+
+coords_module = to_polygon_format(module_vertices_x, module_vertices_y)
+module = Polygon(coords_module)
 ## Define triangular meshgrid ##
 
 nb_rows = 11
@@ -26,7 +51,9 @@ x_inc = 3.1 # [mm] Horizontal increment at each row
 y_inc = 5.369 # [mm] Vertical increment at each row
 # Their norm corresponds to the pitch defined above
 xx = []
+xx1 = np.ones(nb_rows + 1)
 yy = []
+yy1 = np.ones(nb_rows + 1)
 
 for idx in range(nb_rows):
     
@@ -37,9 +64,16 @@ for idx in range(nb_rows):
     xx_new += 5.9
     yy_new = start_offset_y * np.ones(len(xx_new))
     yy_new += 3.41
-    xx.append(list(xx_new))
-    yy.append(list(yy_new))
-    plt.scatter(xx_new, yy_new, marker='o', color='k')
+    xx.append(xx_new.tolist())
+    yy.append(yy_new.tolist())
+
+    if idx == 0:
+         xx1 = xx_new
+         yy1 = yy_new
+    else: 
+         xx1 = np.hstack((xx1, xx_new))
+         yy1 = np.hstack((yy1, yy_new))
+
 
     if idx == nb_rows - 1:
         xx_new = np.array([x_inc * (idx + 1)])
@@ -48,10 +82,11 @@ for idx in range(nb_rows):
         yy_new += 3.41
         xx.append(list(xx_new))
         yy.append(list(yy_new))
-        plt.scatter(xx_new, yy_new, marker='o', color='k')
+        xx1 = np.hstack((xx1, xx_new))
+        yy1 = np.hstack((yy1, yy_new))
 
-xx = np.array(xx, dtype='object')
-yy = np.array(yy, dtype='object')
+list_to_remove = [0, nb_rows, -1]
+xx1, yy1 = remove_positioner(xx1, yy1, list_to_remove)
 
 
 ### Define area for 1 positioner ###
@@ -65,26 +100,49 @@ s2 = np.sin(np.deg2rad(beta))
 
 xa, ya, xab, yab = la*c1, la*s1, (lb+la)*c2, (la+lb)*s2
 
+wks_list = []
 
-for idx, (x_shift, y_shift) in enumerate(zip(xx,yy)):
-    for (x_shifted, y_shifted)in zip(x_shift, y_shift):
-        xa1, ya1, xab1, yab1 = xa + x_shifted, ya + y_shifted, xab + x_shifted, yab + y_shifted
-        coords_int = []
-        coords_ext = []
-        for (x,y) in zip(xa1,ya1):
-            coords_int.append((x,y))
-        for (x,y) in zip(xab1,yab1):
-            coords_ext.append((x,y))
+for idx, (dx, dy) in enumerate(zip(xx1, yy1)):
+
+        xa1, ya1, xab1, yab1 = xa + dx, ya + dy, xab + dx, yab + dy
+
+        coords_int = to_polygon_format(xa1, ya1)
+        coords_ext = to_polygon_format(xab1, yab1)
 
         interior = coords_int[::-1]
         poly_c1 = Polygon(coords_ext, [interior])
-        plot_polygon(poly_c1)
+        wks_list.append(poly_c1)
 
-# interior = coords_a2[::-1]
-# poly_c2 = Polygon(coords_ab2, [interior])
-# plot_polygon(poly_c2)
+total_positioners_area = unary_union(wks_list)
+positioners_area_with_walls = module.intersection(total_positioners_area)
 
-plt.plot(module_vertices_x,module_vertices_y, color='orange')
-plt.grid()
+coverage_with_walls = round(positioners_area_with_walls.area/module.area * 100,1)
+coverage_no_walls = round(total_positioners_area.area/module.area * 100,1)
+
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot()
+plt.title("Module with raw positioner coverage")
+plot_polygon(module, facecolor='None', edgecolor='black', add_points=False)
+plt.scatter(xx1,yy1, marker='.', color='k')
+for idx, wks in enumerate(wks_list):
+    if idx == 0:
+         label_cov = "Coverage: {} %".format(coverage_with_walls)
+    else:
+         label_cov = None
+    plot_polygon(wks, add_points=False, alpha=0.2, facecolor='red', edgecolor='black', label = label_cov)
+plt.xlabel('x position [mm]')
+plt.ylabel('y position [mm]')
+plt.legend(shadow = True)
+
+# plt.figure(figsize=(8,8))
+# plt.title("Module with summed coverage & walls")
+# plot_polygon(module, facecolor='None', edgecolor='black', add_points=False)
+# plt.scatter(xx1,yy1, marker='.', color='k')
+# plot_polygon(coverage_walls, add_points=False, alpha=0.2, edgecolor='black', label = "Coverage: {} %".format(coverage_with_walls))
+# plt.legend()
+
+
+print(coverage_no_walls)
+
 if draw:
     plt.show()
