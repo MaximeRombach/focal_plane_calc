d3973eccd461cba70aad25b8244a892c97a9f90f
MaximeRombach <rombach.maxime@gmail.com>
2023-08-23 11:45:32 +0200

    Read Ansys results structure works

--- read_Ansys_results.py
+++ read_Ansys_results.py
@@ -1,10 +1,20 @@
 import csv
 import matplotlib.pyplot as plt
 import pandas as pd
+import parameters as param
 
-filename = "./Results/turbo_table_data2.csv"
+
+filename = "./Results/turbo_table_data3.csv"
 comment_prefix = "#"  # Set this to the comment prefix used in your file
 separator = ","
+
+save_plots = False
+save_frame_as_dxf = False
+save_csv = False
+save_txt = False
+
+saving_df = {"save_plots": save_plots, "save_dxf": save_frame_as_dxf, "save_csv": save_csv, "save_txt": save_txt}
+saving = param.SavingResults(saving_df)
 
 data = []
 
@@ -26,14 +36,30 @@ print(df)
 
 # Now you can extract columns from the data and create a plot
 x = df['Frame thickness'].astype(float).to_numpy()
-print(type(x[0]))
+
+figtitle = "Max deformation of loaded focal plate VS plate thickness"
+filename = "Max_def_VS_frame_thickness"
 plt.figure(figsize=(12,8))
-plt.plot(df['Frame thickness'].astype(float).to_numpy(), df['def63'].astype(float).to_numpy()*10**6, '.-', label='63 robots/module')
-plt.plot(df['Frame thickness'].astype(float).to_numpy(), df['def75'].astype(float).to_numpy()*10**6, '.-', label='75 robots/module')
-plt.plot(df['Frame thickness'].astype(float).to_numpy(), df['def102'].astype(float).to_numpy()*10**6, '.-', label='102 robots/module')
+plt.plot(x, df['def63'].astype(float).to_numpy()*10**6, '.-', label='63 robots/module')
+plt.plot(x, df['def75'].astype(float).to_numpy()*10**6, '.-', label='75 robots/module')
+plt.plot(x, df['def102'].astype(float).to_numpy()*10**6, '.-', label='102 robots/module')
 plt.xlabel("Frame thickness [mm]")
-plt.ylabel(r'Max deformation [$\mu$m]')
-plt.title("Max deformation of loaded focal plate VS plate thickness")
+plt.ylabel(r"Max deformation [$\mu$m]")
+plt.title(figtitle)
 plt.grid(True)
 plt.legend()
+saving.save_figures_to_dir(filename)
+
+figtitle = "Max stress on loaded focal plate VS plate thickness"
+filename = "Max_stress_VS_frame_thickness"
+plt.figure(figsize=(12,8))
+# plt.plot(x, df['def63'].astype(float).to_numpy()*10**-6, '.-', label='63 robots/module')
+plt.plot(x, df['stress75'].astype(float).to_numpy()*10**-6, '.-', label='75 robots/module')
+plt.plot(x, df['stress102'].astype(float).to_numpy()*10**-6, '.-', label='102 robots/module')
+plt.xlabel("Frame thickness [mm]")
+plt.ylabel("Max stress [MPa]")
+plt.title(figtitle)
+plt.grid(True)
+plt.legend()
+saving.save_figures_to_dir(filename)
 plt.show()
