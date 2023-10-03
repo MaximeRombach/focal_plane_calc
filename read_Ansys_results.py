84fd5710d622aac35f6c95c83361240a12dd4751
MaximeRombach <rombach.maxime@gmail.com>
2023-10-03 17:24:59 +0200

    Read Ansys log file direct and skip first rows

--- read_Ansys_results.py
+++ read_Ansys_results.py
@@ -22,34 +22,34 @@ Output:
     - TBD: Figure: frame thickness vs average stress (max stress is influenced by stress concentration, good to have average stress to check coherence)
 
 """
-filename = "./Results/turbo_table_data5.csv"
+# Fileneame = path to log file from Ansys analyses
+filename = "C:/Users/rombach/Documents/Astrobots/Inosuisse/FEM/True_frame/true_frame_test_files/user_files/DesignPointLog.csv"
 comment_prefix = "#"  # Set this to the comment prefix used in your file
 separator = ","
 
 save_plots = False
-save_frame_as_dxf = False
-save_csv = False
-save_txt = False
+num_lines_to_skip = 198 # log file logs since beginning of analyses, skip first rows that are now irrelevant
 
-saving_df = {"save_plots": save_plots, "save_dxf": save_frame_as_dxf, "save_csv": save_csv, "save_txt": save_txt}
+saving_df = {"save_plots": save_plots}
 saving = param.SavingResults(saving_df)
 
 data = []
 
 with open(filename, "r") as csv_file:
     csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-        if not row or row[0].startswith(comment_prefix):
+    for line_number, row in enumerate(csv_reader):
+        if not row or row[0].startswith(comment_prefix) or line_number <= num_lines_to_skip:
             continue  # Skip empty rows and comment lines
         data.append(row)
 
 # Now the 'data' list contains the non-comment CSV rows
 
 df = pd.DataFrame(data)
-df.drop([0,1],inplace=True)
-df.drop([0,1,2], axis=1, inplace=True)
 print(df) 
-df.columns = ['Frame thickness', 'def63', 'def102', 'maxstress102', 'maxstress63', 'avstress63', 'avstress102', 'def75', 'maxstress75', 'avstress75']
+df.drop([0],inplace=True)
+df.drop([0,3,6], axis=1, inplace=True)
+print(df) 
+df.columns = ['Frame thickness', 'maxstress75', 'maxstress102', 'avstress102', 'def63', 'maxstress63', 'avstress63', 'def102', 'avstress75', 'def75']
 df.sort_values(by=['Frame thickness'], ascending = True, inplace = True)
 
 
@@ -77,6 +77,8 @@ plt.plot(x, df['maxstress75'].astype(float).to_numpy()*10**-6, '.-', label='75 r
 plt.plot(x, df['maxstress102'].astype(float).to_numpy()*10**-6, '.-', label='102 robots/module')
 plt.xlabel("Frame thickness [mm]")
 plt.ylabel("Max stress [MPa]")
+
+# plt.tick_params(which='minor', length=5, width=2)
 plt.title(figtitle)
 plt.grid(True)
 plt.legend()
