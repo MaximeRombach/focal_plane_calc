9171eb5c6fae3216f5983ccb26e6108d7d6df267
Izumi_V_Reinherz <aidenxu1998@hotmail.com>
2023-12-15 15:06:04 +0100

    Patched an numeric to string parsing error that prevents adding global variables

--- SolidworksAutomationTool/SolidworksAutomationTool/ScaffoldFunctions.cs
+++ SolidworksAutomationTool/SolidworksAutomationTool/ScaffoldFunctions.cs
@@ -599,11 +599,15 @@ namespace SolidworksAutomationTool
         public static int CreateGlobalVariableInAllConfigs(ref ModelDoc2 partModelDoc, string variableName, double variableValue)
         {
             EquationMgr equationManager = partModelDoc.GetEquationMgr();
+            // Parse the numeric value of variable into a string. Default to preserve 6 digits after the decimal point
+            // Using the culture neutral way of representing numbers: e.g. 123.456 instead of 123,456
+            string parsedVariableValue = variableValue.ToString("0.000000", invariantCultureInfo);
+            // Create the expression used to add global variables.The variable is added with meters as unit
+            string variableAssignmentExpression = $"\"{variableName}\" = {parsedVariableValue}m";
             // syntax is referenced from https://help.solidworks.com/2022/English/api/sldworksapi/Add_Equations_Example_CSharp.htm?verRedirect=1
             // by default adding the global variable to the end of the equation list
-            //int variableIndex = equationManager.Add3(-1, $"\"{variableName}\" = {variableValue}m", true, (int)swInConfigurationOpts_e.swAllConfiguration, null);
-            string variableAssignmentExpression = $"\"{variableName}\" = {variableValue,0:F6}m";
             int variableIndex = equationManager.Add2(-1, variableAssignmentExpression, true);
+
             // TODO: check if the feature manager update is necessary
             partModelDoc.FeatureManager.UpdateFeatureTree();
             if (variableIndex == -1)
