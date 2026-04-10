df01dc3ce8caf3d3b978d6c95a58970842a63e72
MaximeRombach <rombach.maxime@gmail.com>
2026-04-10 14:48:05 +0200

    BFS calc function

--- Focal_Suface.py
+++ Focal_Suface.py
@@ -8,6 +8,7 @@ import numpy as np
 from shapely.geometry import Polygon
 import json
 from scipy.interpolate import interp1d
+from scipy.optimize import least_squares
 from shapely.plotting import plot_polygon
 import matplotlib.pyplot as plt
 
@@ -42,11 +43,14 @@ class FocalSurf:
         self.focus_tolerance_width = kwargs.get('focus_tolerance_width', None) # [mm] tolerance on the focal surface
         self.plate_scale = kwargs.get('focus_tolerance_width', None) # [um/arcsec] corresponding distance on the focal surface mapped to angular position on sky
         self.donut_diam = kwargs.get('donut_diam', None) # [um/arcsec] corresponding distance on the focal surface mapped to angular position on sky
-    
+
+        self.c = 1 / abs(self.curvature_R) # [1/mm] curvature of the focal surface
+
         self.optics_data = self.optics_data() # pandas dataframe containing the focal surface data from Zemax csv file
 
         self.trimming_angle = kwargs.get('trimming_angle', 360) # [deg] angle to trim the vignetting disk at
-    
+        self.R2Z, self.R2CRD, self.R2NORM, self.R2NUT, self.S2R = self.transfer_functions() # transfer functions to convert radius to height on focal surface, get chief ray deviation interms of radial position on focal surface, map the radial position to the angle of the normal vector of the focal surface and map the radial position to the actual nutation angle which is: R2NORM + CRD
+
     def optics_data(self, alt_file_name: str = None):
 
         if alt_file_name is not None:
@@ -85,7 +89,7 @@ class FocalSurf:
         return optics_data
     
     @property
-    def vignetting_disk(self, trimming_angle: int = 180, changed_vigR: float = None):
+    def vignetting_disk(self, changed_vigR: float = None):
         """
         Returns the vignetting disk as a shapely Polygon object
         
@@ -100,6 +104,9 @@ class FocalSurf:
             vigR = changed_vigR # [mm] allows to change the vignetting radius for specific cases
         else:
             vigR = self.vigR # [mm] vignetting radius, nominal case declared for each project
+
+        if self.trimming_angle is None:
+            self.trimming_angle = 360
 
         vigR_lim_x = vigR * np.cos(np.deg2rad(np.linspace(0,self.trimming_angle,500)))
         vigR_lim_y = vigR * np.sin(np.deg2rad(np.linspace(0,self.trimming_angle,500)))
@@ -159,23 +166,24 @@ class FocalSurf:
             # R2Z maps the radial position on the focal plane to the height of the focal plane
             if 'Z' in self.optics_data.keys():
                 Z = self.optics_data['Z']
-                R2Z = interp1d(R,Z,kind='cubic', fill_value = "extrapolate") #leave 'cubic' interpolation for normal vectors calculations
+                # R2Z = interp1d(R,Z,kind='cubic', fill_value = "extrapolate")
+                R2Z = interp1d(R,Z,kind='cubic') #leave 'cubic' interpolation for normal vectors calculations
             else:
                 R2Z = lambda r: self.BFS - np.sqrt(self.BFS**2 - r**2) # Spherical focal surface
                 logging.warning('No Z data available in samples - assuming SPHERICAL surface')
 
         else:
             logging.info('Transfer functions: using aspherical formula cofficients, no CRD data available')
-            R2Z = self.analytical_R2Z
+            R2Z = self.analytical_R2Z()
 
         """ R2CRD """
         if self.is_CRD:
             CRD = self.optics_data['CRD']
             R = self.optics_data['R']
-            R2CRD = interp1d(R,CRD,kind='cubic', fill_value = "extrapolate")
+            R2CRD = interp1d(R,CRD,kind='cubic')
         else:
             CRD = np.zeros_like(R)
-            R2CRD = interp1d(R,CRD,kind='cubic', fill_value = "extrapolate")
+            R2CRD = interp1d(R,CRD,kind='cubic')
         crd = R2CRD(r)
 
         """ Calculate derivative of focal surface """
@@ -183,22 +191,65 @@ class FocalSurf:
         dr = np.diff(r)
         dz = np.diff(z)
         dzdr = dz/dr
+        dzdr = np.insert(dzdr, 0, 0)
 
         """ R2NORM """
         norm = np.degrees(np.arctan(dzdr))# Calculate normal angles at the sample points
-        R2NORM = interp1d(r[:-1], norm,kind='cubic', fill_value = "extrapolate")
+        R2NORM = interp1d(r, norm,kind='cubic')
 
         """ R2NORM """
+        print("R = ", r)
         nut = R2NORM(r) + crd # Joe: -(R2NORM(r) + crd)
-        R2NUT = interp1d(r, nut, kind='cubic', fill_value = "extrapolate")
+        R2NUT = interp1d(r, nut, kind='cubic', fill_value="extrapolate")
 
         # Parametric path length along the focal surface
-        ds = (1 + dzdr**2)**0.5 * dr
+        ds = (1 + dzdr[1:]**2)**0.5 * dr
         s = np.cumsum(ds)
         s = np.insert(s, 0, 0.)  # first value of path length is 0
-        S2R = interp1d(s, r, kind='cubic', fill_value = "extrapolate")
+        S2R = interp1d(s, r, kind='cubic', fill_value="extrapolate")
 
         return R2Z, R2CRD, R2NORM, R2NUT, S2R
+    
+    def analytical_R2Z(self):
+        """ 
+        Returns the aspherical focal plane curve from the aspherical coefficients
+
+        Input:
+                  - r: [np.array] radial positions on focal plane
+        Output:   
+                  - z: [np.array] height positions on focal plane
+        """
+        print(f"Using aspherical formula with coefficients: c={self.c}, k={self.k}, a2={self.a2}, a4={self.a4}, a6={self.a6}, a8={self.a8}")
+        return lambda r: (r**2 * self.c) / (1 + np.sqrt(1 - (1 + self.k) * (r**2 * self.c**2))) + self.a2 * r**2 + self.a4 * r**4 + self.a6 * r**6 + self.a8 * r**8 # analytical formula to get Z from R for aspheric surfaces, if asph_formula is True
+
+    def BFS_radius(self):
+        
+        # Estimate the best fit sphere radius from the sampled data, using least squares optimization
+        # Courtesy from Joe Silber (LBNL) and his code in https://github.com/joesilber/raft-design
+        r = np.linspace(0,self.vigR,2000)
+        z = self.R2Z(r)
+        typical_fov = self.FoV  # [deg]
+        z_guess = np.sign(np.mean(z)) * np.max(r) / np.radians(typical_fov/2)
+        print(f"Initial guess for BFS radius: {z_guess:.3f} mm")
+        result = least_squares(fun=self.calc_sphR_err, x0=z_guess)
+        BFS_calc = float(result.x)
+        BFS_calc = abs(BFS_calc)
+        is_convex = np.sign(BFS_calc) < 1  # convention where +z is toward the fiber tips
+        sphR_sign = -1 if is_convex else +1
+        logging.info(f'Best-fit sphere radius = {BFS_calc:.3f} mm, is_convex = {is_convex}')
+
+        return BFS_calc
+
+    def calc_sphR_err(self, z_ctr):
+        calc_sphR = lambda z_ctr: (r**2 + (z - z_ctr)**2)**0.5
+        sphR_test = calc_sphR(z_ctr)
+        errors = sphR_test - np.mean(sphR_test)
+        scalar_error = np.sum(np.power(errors, 2))
+        return scalar_error
+    
+    def R2BFS(self, r):
+        BFS = self.BFS_radius()
+        return BFS - np.sqrt(BFS**2 - r**2)
     
     def trimming_polygon(self, geometry = 'hex', trim_diff_to_vigR: float = 0):
         """
@@ -273,14 +324,35 @@ if __name__ == "__main__":
     plot_polygon(surf.vignetting_disk) # Should print the vignetting disk as a shapely Polygon object
     plot_polygon(surf.trimming_polygon(geometry='hex')) # Should print the trimming polygon as a shapely Polygon object
     
-    figure, ax = plt.subplots()
     r = np.linspace(0,surf.vigR,500)
+
+    figure, ax = plt.subplots()
     z = R2Z(r)
-    plt.plot(r, z)
+    z_BFS = surf.R2BFS(r)
+
+    plt.plot(r, z, label='Real Focal Surface')
+    plt.plot(r, z_BFS, '--', label=f'BFS radius:{surf.BFS_radius():.2f} mm')
     plt.xlabel('Radial position on focal surface (R) [mm]')
     plt.ylabel('Height on focal surface (Z) [mm]')
+    plt.title(f'{surf.project_name} - Focal Surface Profile')
+    plt.legend(loc='upper left')
     plt.grid()
-    
+
+    plt.figure()
+    plt.plot(r, z-z_BFS, )
+    plt.xlabel('Radial position on focal surface (R) [mm]')
+    plt.ylabel('Error BFS to Focal surface [mm]')
+    plt.grid()
+    plt.title(f'{surf.project_name} - Error between BFS and real focal surface')
+
+    plt.figure()
+    crd = R2CRD(r)
+    plt.plot(r, crd)
+    plt.xlabel('Radial position on focal surface (R) [mm]')
+    plt.ylabel('Chief Ray Deviation (CRD) [deg]')
+    plt.title(f'{surf.project_name} - CRD as a function of radial position on focal surface')
+    plt.grid()
+
     plt.show()
 
     
